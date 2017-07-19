bd69cb69e6684ab44ff8b1a9ea55affaa1ce22e6
Olmanc <oruman.247@gmail.com>
2017-07-18 19:38:08 -0600

    feat: added file for fetch from api

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+function fetchStocks(func="TIME_SERIES_DAILY",stock){
+    var encodedURI = window.encodeURI('https://www.alphavantage.co/query?function='+func+'&symbol='+stock+'&apikey=K7S08RRI532JTVS3');
+    return axios.get(encodedURI)
+    .then( (response)=>{
+        console.log('fetch')
+        //console.log(response.data['Time Series (Daily)'])
+        switch(func){
+            case "TIME_SERIES_DAILY":
+                return response.data['Time Series (Daily)'];
+            case "TIME_SERIES_WEEKLY":
+                return response.data['Weekly Time Series'];
+            case "TIME_SERIES_MONTHLY":
+                return response.data['Monthly Time Series'];
+            default:
+                return response.data['Time Series (Daily)'];
+        }
+        //return response.data['Time Series (Daily)'];
+    })
+}
+
+export default {fetchStocks};
