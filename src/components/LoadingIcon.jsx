8caabbba7fa1b25b01f830af1841e7b617b667aa
amlopez333 <amlopez333@gmail.com>
2017-07-23 03:47:25 -0600

    feat: added loading icon to login and portfolio

---
+++ src/components/LoadingIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const LoadingIcon = React.createClass({
+    render: function(){
+        return(
+            <img src = './loading.jpg' alt="" className="App-logo"/>
+        );
+    }
+});
+export default LoadingIcon;
